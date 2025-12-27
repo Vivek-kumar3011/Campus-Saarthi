ffa1abcb762cb5c6a335da34515c33af5b84ce09
Vivek-Singh-2006 <vivek8414051@gmail.com>
2025-12-27 19:53:26 +0530

    Add file for app version

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  base: './', // 👈 YE SABSE ZAROORI HAI! Isse path /assets se ./assets ho jayega.
+  build: {
+    outDir: 'dist', // 👈 Confirm karein ki output 'dist' folder mein ja raha hai
+    emptyOutDir: true,
+  }
 })
